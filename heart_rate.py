22106762fc7d78193537601ce8b4e98d36e300a8
RommelJuarez <jua22005@byui.edu>
2022-09-24 17:56:03 -0500

    hola

---
+++ heart_rate.py
@@ -0,0 +1,20 @@
+"""
+When you physically exercise to strengthen your heart, you
+should maintain your heart rate within a range for at least 20
+minutes. To find that range, subtract your age from 220. This
+difference is your maximum heart rate per minute. Your heart
+simply will not beat faster than this maximum (220 - age).
+When exercising to strengthen your heart, you should keep your
+heart rate between 65% and 85% of your heart's maximum rate.
+"""
+import os
+os.system("cls")
+
+age=int(input("Please enter your age:"))
+maximun_beat=(220-age)
+minimun_rate=int(maximun_beat*0.65)
+maximun_rate=int(maximun_beat*0.85)
+print(f"""When you exercise to strengthen your heart, you should 
+keep your heart rate between {minimun_rate} and {maximun_rate} beats per minute.""")
+
+
